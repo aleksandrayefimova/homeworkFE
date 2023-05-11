81cbe14009c63e686515a56930c5c4b82ca6fcff
aleksandrayefimova <123464076+aleksandrayefimova@users.noreply.github.com>
2023-05-11 17:28:31 +0200

    hw 4,5

---
+++ FE hw5/script.js
@@ -0,0 +1,32 @@
+// задание 1
+// const num = prompt('введите число');
+// console.log(num*0.1);
+
+// задание 2
+// const num1 = prompt('введите число');
+// const num2 = prompt('введите число');
+// if (num1>num2) {
+//     console.log(num2);   
+// } else {
+//     console.log(num1);
+// };
+
+// задание 3
+// const num = prompt('введите число');
+// if (num>100) {
+//    console.log('число больше 100'); 
+// } else if (num<100) {
+//     console.log('число меньше 100');
+// } else {
+//     console.log('число равно 100');
+// }
+
+// задание 4
+const name = prompt('введите имя');
+const age = prompt('введите возраст');
+ if (age>=18) {
+    console.log(`Hello, ${name}`);
+ }
+ else {
+    console.log(`Hi, ${name}`);
+ }
